8cc2f2a37055247e635560d098de8b58ce1e1413
johntduong <johntduong@yahoo.com>
2018-02-02 00:08:06 -0800

    app can load and land on home page

--- datart/js/App.jsx
+++ datart/js/App.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+// @flow
+
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import type { Match } from "react-router-dom";
 
 const App = () => (
-  return (
-    <h1>Hello</h1>
-  )
+  <div className="app">
+    <h1>Hello World</h1>
+  </div>
 );
 
 export default App;
--- datart/js/ClientApp.jsx
+++ datart/js/ClientApp.jsx
@@ -1,10 +1,20 @@
+// @flow
+
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-
+import Perf from "react-addons-perf";
 import App from "./App";
+
+window.Perf = Perf;
+Perf.start();
 
 const renderApp = () => {
   render(<App />, document.getElementById("app"));
 };
 renderApp();
+
+if (module.hot) {
+  module.hot.accept("./App", () => {
+    renderApp();
+  });
+}
