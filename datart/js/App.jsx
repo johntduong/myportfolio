6fddedd45a38311659d9ec790dccd61ca21c1dfd
johntduong <johntduong@yahoo.com>
2018-02-05 22:18:46 -0800

    created header component and landing component

--- datart/js/App.jsx
+++ datart/js/App.jsx
@@ -2,12 +2,19 @@
 
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import type { Match } from "react-router-dom";
+import Landing from "./Landing";
+
+const FourOhFour = () => <h1>404</h1>;
 
 const App = () => (
-  <div className="app">
-    <h1>Hello World</h1>
-  </div>
+  <BrowserRouter>
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route component={FourOhFour} />
+      </Switch>
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
---
+++ datart/js/Landing.jsx
@@ -0,0 +1,15 @@
+// @flow
+
+import React from "react";
+import Header from "./Header";
+
+const Landing = () => {
+  return (
+    <div className="landing">
+      <Header />
+
+    </div>
+  );
+};
+
+export default Landing;
