326689a786bb667cbfa73689443624bf96a80dc8
johntduong <johntduong@yahoo.com>
2018-02-06 23:50:52 -0800

    restarted project with adjusted packages and configs

--- datart/webpack.config.js
+++ datart/webpack.config.js
@@ -1,49 +1,24 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
 
 module.exports = {
-  context: __dirname,
-  entry: [
-    "react-hot-loader/patch",
-    "webpack-dev-server/client?http://localhost:8080",
-    "webpack/hot/only-dev-server",
-    "./js/ClientApp.jsx"
-  ],
-  devtool: "cheap-eval-source-map",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js",
-    publicPath: "/public/"
-  },
-  devServer: {
-    hot: true,
-    publicPath: "/public/",
-    historyApiFallback: true
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".json"]
-  },
-  stats: {
-    colors: true,
-    reasons: true,
-    chunks: true
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin()
-  ],
-  module: {
-    rules: [
-      {
-        enforce: "pre",
-        test: /\.jsx?$/,
-        loader: "eslint-loader",
-        exclude: /node_modules/
-      },
-      {
-        test: /\.jsx?$/,
-        loader: "babel-loader"
-      }
-    ]
-  }
+    context: __dirname,
+    entry: './js/ClientApp.jsx',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    devServer: {
+        publicPath: '/public/'
+    }
 };
