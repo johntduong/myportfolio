580bad3c4cfbbed84ebc33322350e1472d53fbaf
johntduong <johntduong@yahoo.com>
2018-02-07 22:16:16 -0800

    added artwork, contact, and home component

--- datart/js/ClientApp.jsx
+++ datart/js/ClientApp.jsx
@@ -1,6 +1,25 @@
 import React from "react";
 import { render } from "react-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Home from "./Home";
+import Artwork from "./Artwork";
+import Contact from "./Contact";
 
-const ClientApp = () => ( <h1>Hello There</h1>);
+const FourOhFour = () => <h1>404</h1>;
+
+const ClientApp = () => {
+    return ( 
+        <BrowserRouter>
+            <div className="app">
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/artwork" component={Artwork} />
+                    <Route exact path="/contact" component={Contact} />
+                    <Route component={FourOhFour} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    )
+};
 
 render(<ClientApp/>, document.getElementById("app"));
