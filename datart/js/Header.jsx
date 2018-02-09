68ff07a7edf4a254723061de77eae81ae2d4cf05
johntduong <johntduong@yahoo.com>
2018-02-08 23:19:03 -0800

    added styling for Header component

--- datart/js/Header.jsx
+++ datart/js/Header.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
 import AppBar from "material-ui/AppBar";
 
+const muiTheme = getMuiTheme({
+  palette: {
+    color: "blueGrey500"
+  },
+  appBar: {
+    height: 50
+  }
+});
+
 const Header = () => (
-  <MuiThemeProvider>
+  <MuiThemeProvider muiTheme={muiTheme}>
     <AppBar
       title="Wall Sculpture and Granite"
       iconClassNameRight="muidocs-icon-navigation-expand-more"
