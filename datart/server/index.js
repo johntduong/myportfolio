8c427b9f1625d4df323ea2afa41767815e9dc05a
johntduong <johntduong@yahoo.com>
2018-02-14 22:41:10 -0800

    added route and nodemailer package

--- datart/server/index.js
+++ datart/server/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const nodemailer = require("nodemailer");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+const sendEmail = () => {};
+
+app.post("/email", sendEmail);
 
 const port = 1337;
 app.listen(port);
