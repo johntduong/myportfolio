28dffb9f532ebb5ee27ff087e26b40e7fe2752c7
johntduong <johntduong@yahoo.com>
2018-02-16 23:44:20 -0800

    setting up nodemailer, adding main transport

--- datart/server/index.js
+++ datart/server/index.js
@@ -3,6 +3,20 @@ const bodyParser = require("body-parser");
 const app = express();
 const nodemailer = require("nodemailer");
 
+nodemailer.createTestAccount() => {
+  let transporter = nodemailer.createTransport();
+  let mailOptions = {
+
+  }
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview Url: %s', nodemailer.getTestMessageUrl(info));
+  })
+}
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
