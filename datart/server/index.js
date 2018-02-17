e3ab0d67962bf1b3a3a61a8ea9fa33e69566f292
johntduong <johntduong@yahoo.com>
2018-02-17 00:07:13 -0800

    set up smtp config for gmail and server can now send emails

--- datart/server/index.js
+++ datart/server/index.js
@@ -3,26 +3,45 @@ const bodyParser = require("body-parser");
 const app = express();
 const nodemailer = require("nodemailer");
 
-nodemailer.createTestAccount() => {
-  let transporter = nodemailer.createTransport();
-  let mailOptions = {
+// Generate test SMTP service account from ethereal.email
+// Only needed if you don't have a real mail account for testing
+nodemailer.createTestAccount((err, account) => {
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // use SSL
+    auth: {
+      user: "datduongart@gmail.com", // generated ethereal user
+      pass: "datloan2949@" // generated ethereal password
+    }
+  });
 
-  }
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: '"John Duong" <datduongart@gmail.com>', // sender address
+    to: "datduongart@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello from node?", // plain text body
+    html: "<b>Hello world?</b>" // html body
+  };
+
+  // send mail with defined transport object
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       return console.log(error);
     }
-    console.log('Message sent: %s', info.messageId);
-    console.log('Preview Url: %s', nodemailer.getTestMessageUrl(info));
-  })
-}
+    console.log("Message sent: %s", info.messageId);
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  });
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-const sendEmail = () => {};
-
-app.post("/email", sendEmail);
 
 const port = 1337;
 app.listen(port);
